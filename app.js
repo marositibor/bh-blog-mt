32bb70bf694f7820b2b560925debbb9c2d937281
Marosi Tibor <marositibor@Marosi-MacBook-Air.local>
2020-03-14 10:39:50 +0100

    post list layout

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
+const sqlite3 = require("sqlite3").verbose();
+const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
+const PORT = 3000;
 const app = express();
-const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("handlebars", hbs());
+app.set("view engine", "handlebars");
 app.use(cookieParser());
 app.use(express.static("public"));
 
-app.post("/login", LoginController.post);
+const header = "bh-blog";
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const postList = [{ 
+    author: "admin", 
+    date: "2020.01.01",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum congue elementum. Quisque non semper risus, bibendum faucibus odio. Ut eu dolor vel erat varius rutrum eu in massa. Aliquam semper, velit eu tempus lobortis, ex justo sagittis sapien, vitae venenatis purus odio sit amet diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus turpis ante, sollicitudin viverra molestie sit amet, lobortis eget purus. Etiam ac ultrices mauris." 
+},
+{ 
+    author: "admin", 
+    date: "2020.01.01",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum congue elementum. Quisque non semper risus, bibendum faucibus odio. Ut eu dolor vel erat varius rutrum eu in massa. Aliquam semper, velit eu tempus lobortis, ex justo sagittis sapien, vitae venenatis purus odio sit amet diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus turpis ante, sollicitudin viverra molestie sit amet, lobortis eget purus. Etiam ac ultrices mauris." 
+},
+{ 
+    author: "admin", 
+    date: "2020.01.01",
+    title: "Lorem ipsum",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum fermentum congue elementum. Quisque non semper risus, bibendum faucibus odio. Ut eu dolor vel erat varius rutrum eu in massa. Aliquam semper, velit eu tempus lobortis, ex justo sagittis sapien, vitae venenatis purus odio sit amet diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus turpis ante, sollicitudin viverra molestie sit amet, lobortis eget purus. Etiam ac ultrices mauris." 
+}];
+
+app.get("/", (req, res) => {
+  res.render("postlist", {
+    header: header,
+    postList: postList
+  });
+});
+
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
--- controllers/login-controllers.js
+++ controllers/login-controllers.js
@@ -1,17 +1,16 @@
 module.exports = class LoginController {
-    post(req, res) => {
+    post(req, res) {
         const loginService = new loginService()
     
-    
         //1. user adatok ellenőrzése
-        const user = loginService.getAuthenticatedUser(req.username, req.password)
+        const user = loginService.getAuthenticatedUser(req.username, req.password);
         if (!user){
             res.send("user not found")
             return;
         }
         
         //2. user session létrehozása ha valid volt az adat
-        const session = registerSession(user)
+        const session = registerSession(user);
         res.cookie("sessid",session.id);
         //3. átirányítás
         res.redirect("/admin");
