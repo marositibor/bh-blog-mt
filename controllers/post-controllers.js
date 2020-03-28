f38e78e4742422dcab4e9c4401e8c75555e62c81
marositibor <57150119+marositibor@users.noreply.github.com>
2020-03-28 10:38:00 +0100

    fixed validation for draft

--- controllers/post-controllers.js
+++ controllers/post-controllers.js
@@ -1,158 +1,155 @@
 const header = "bh-blog";
 
 const {
-  selectAllPosts,
-  selectPublishedPosts,
-  insertPost,
-  selectPostById,
-  selectPostBySlug,
-  updatePost
+	selectAllPosts,
+	selectPublishedPosts,
+	insertPost,
+	selectPostById,
+	selectPostBySlug,
+	updatePost
 } = require("../services/db-service");
 
 module.exports = class PostController {
-  createNewPost(req, res) {
+	createNewPost(req, res) {
+		if (!req.form.isValid) {
+			res.sendStatus(400);
+			return;
+		}
 
-    if (!req.form.isValid) {
-      res.sendStatus(400);
-      return;
-    }
+		const title = req.body.postTitle;
+		const content = req.body.postContent;
+		const slug = req.body.postSlug;
+		const publish = JSON.parse(req.body.publish);
+		const author = req.session.username;
+		console.log("publish: " + publish);
 
-    const title = req.body.postTitle;
-    const content = req.body.postContent;
-    const slug = req.body.postSlug;
-    const publish = JSON.parse(req.body.publish);
-    const author = req.session.username;
-  console.log("publish: " + publish);
+		const post = {
+			author,
+			title,
+			content,
+			slug,
+			publish,
+			created_at: Date.now()
+		};
 
-    const post = {
-      author,
-      title,
-      content,
-      slug,
-      publish,
-      created_at: Date.now()
-    };
+		insertPost(post).then(() => res.redirect("/admin"));
+	}
 
-    insertPost(post).then(() => res.redirect("/admin"));
-  }
+	getNewPost(req, res) {
+		res.render("new_post", {
+			header: header
+		});
+	}
 
-  getNewPost(req, res) {
-    res.render("new_post", {
-      header: header
-    });
-  }
+	getPostList(req, res) {
+		selectPublishedPosts().then(postList => {
+			res.render("postlist", {
+				header: header,
+				postList
+			});
+		});
+	}
 
-  getPostList(req, res) {
-    selectPublishedPosts().then(postList => {
-      res.render("postlist", {
-        header: header,
-        postList
-      });
-    });
-  }
+	getPostListAdmin(req, res) {
+		selectAllPosts().then(postList => {
+			res.render("postlist-admin", {
+				header: header,
+				postList
+			});
+		});
+	}
 
-  getPostListAdmin(req, res) {
-    selectAllPosts().then(postList => {
-      res.render("postlist-admin", {
-        header: header,
-        postList
-      });
-    });
-  }
+	getPost(req, res) {
+		if (!req.params.id) {
+			res.redirect("/");
+			return;
+		}
 
-  getPost(req, res) {
-    if (!req.params.id) {
-      res.redirect("/");
-      return;
-    }
+		if (isNaN(req.params.id)) {
+			selectPostBySlug(req.params.id)
+				.then(post => {
+					res.render("post", {
+						header: header,
+						post
+					});
+				})
+				.catch(() => {
+					res.sendStatus(404);
+				});
+			return;
+		} else {
+			selectPostById(req.params.id)
+				.then(post => {
+					res.render("post", {
+						header: header,
+						post
+					});
+				})
+				.catch(() => {
+					res.sendStatus(404);
+				});
+		}
+	}
 
-    if (isNaN(req.params.id)) {
-      selectPostBySlug(req.params.id)
-        .then(post => {
-          res.render("post", {
-            header: header,
-            post
-          });
-        })
-        .catch(() => {
-          res.sendStatus(404);
-        });
-      return;
-    } else {
-      selectPostById(req.params.id)
-        .then(post => {
-          res.render("post", {
-            header: header,
-            post
-          });
-        })
-        .catch(() => {
-          res.sendStatus(404);
-        });
-    }
-  }
+	editPost(req, res) {
+		if (!req.form.isValid) {
+			res.sendStatus(400);
+			return;
+		}
 
-  editPost(req, res) {
+		const id = req.params.id;
+		const title = req.body.postTitle;
+		const content = req.body.postContent;
+		const slug = req.body.postSlug;
+		const author = req.session.username;
+		const publish = JSON.parse(req.body.publish);
 
-    if (!req.form.isValid) {
-      res.sendStatus(400);
-      return;
-    }
+		const post = {
+			id,
+			author,
+			title,
+			content,
+			slug,
+			publish,
+			created_at: Date.now()
+		};
 
-    const id = req.params.id;
-    const title = req.body.postTitle;
-    const content = req.body.postContent;
-    const slug = req.body.postSlug;
-    const author = req.session.username;
-    const publish = JSON.parse(req.body.publish);
+		updatePost(post).then(() => res.redirect("/admin/post_list"));
+	}
 
+	getEditPost(req, res) {
+		selectPostById(req.params.id)
+			.then(post => {
+				res.render("edit_post", {
+					header: header,
+					post
+				});
+			})
+			.catch(() => {
+				res.sendStatus(404);
+			});
+	}
 
-    const post = {
-      id,
-      author,
-      title,
-      content,
-      slug,
-      publish,
-      created_at: Date.now()
-    };
+	validateCreateNewPost(req, res, next) {
+		req.form = {};
+		const invalidFields = [];
+		const publish = req.body.publish ? JSON.parse(req.body.publish) : true;
 
-    updatePost(post).then(() => res.redirect("/admin/post_list"));
-  }
+		if (req.body.postTitle == undefined || req.body.postTitle == "") {
+			invalidFields.push("Title is required!");
+		}
 
-  getEditPost(req,res){
-    selectPostById(req.params.id)
-    .then(post => {
-      res.render("edit_post", {
-        header: header,
-        post
-      });
-    })
-    .catch(() => {
-      res.sendStatus(404);
-    });
-  }
+		if (req.body.postContent == undefined || req.body.postContent == "") {
+			invalidFields.push("Content is required!");
+		}
 
-  validateCreateNewPost(req, res, next) {
-    req.form = {};
-    const invalidFields = [];
-    const publish = req.body.publish ? JSON.parse(req.body.publish) : true;
-
-    if (req.body.postTitle == undefined || req.body.postTitle == "") {
-      invalidFields.push("Title is required!");
-    }
-
-    if (req.body.postContent == undefined || req.body.postContent == "") {
-      invalidFields.push("Content is required!");
-    }
-
-    if (publish == false || invalidFields.length == 0) {
-      req.form.isValid = true;
-      next();
-    } else {
-      req.form.isValid = false;
-      req.form.validationMessage = invalidFields;
-      next;
-    }
-  }
+		if (publish == false || invalidFields.length == 0) {
+			req.form.isValid = true;
+			next();
+		} else {
+			req.form.isValid = false;
+			req.form.validationMessage = invalidFields;
+			next;
+		}
+	}
 };
